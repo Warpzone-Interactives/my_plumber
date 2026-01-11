0be925580ba827e2c035e8a299594bf43c8640b5
Sweton Herbin <sweton.herbin@epitech.eu>
2026-01-11 14:26:31 +0100

    add : keypressed event in code (Escape end the game)

--- window.cpp
+++ window.cpp
@@ -7,18 +7,42 @@
 
 #include "my.hpp"
 
-void analyse_events(sf::RenderWindow *window)
+void key_event(sf::RenderWindow *window, sf::Event *event)
+{
+    switch (event->key.code) {
+        case sf::Keyboard::Escape:
+            window->close();
+            break;
+        default:
+            return;
+    }
+}
+
+void analyse_events(sf::Event *event, sf::RenderWindow *window)
+{
+    switch (event->type) {
+        case sf::Event::KeyPressed:
+            key_event(window, event);
+            break;
+        case sf::Event::Closed:
+            window->close();
+            break;
+        default:
+            return;
+    }
+}
+
+void poll_event(sf::RenderWindow *window)
 {
     sf::Event event;
 
     while (window->pollEvent(event))
-        if (event.type == sf::Event::Closed)
-            window->close();
+        analyse_events(&event, window);
 }
 
 void handle_window(sf::RenderWindow *window, std::vector<sf::Sprite> &sprites)
 {
-    analyse_events(window);
+    poll_event(window);
     window->clear();
     for (const sf::Sprite &s : sprites)
         window->draw(s);
