4d59f6e27a607e6e5cafbc144e3f7cbd24b2afb5
Ternal <mathew.duhamel80850@gmail.com>
2026-01-29 18:42:47 +0100

    [+] Reorganized the player.cpp and added intRect depending on size.

--- player/player.cpp
+++ player/player.cpp
@@ -23,12 +23,83 @@ player::player(int size, sf::Vector2f position, char m)
     _facingRight = true;
     _running = false;
     _chooseTexture();
-    _rect = sf::IntRect({0, 0}, {16, 16});
     _sprite = sf::Sprite(_texture);
     _sprite.setTextureRect(_rect);
     _sprite.setPosition(_position);
     _sprite.setOrigin({8, 8});
 }
+
+void player::_chooseTexture()
+{
+    std::string filePath = "ressources/player/" + _character + "/";
+    
+    if (_size == 0) {
+        filePath += "small.png";
+        _rect = sf::IntRect({0, 0}, {16, 16});
+    }
+    else if (_size == 1) {
+        filePath += "big.png";
+        _rect = sf::IntRect({0, 0}, {16, 32});
+    }
+    else if (_size == 2) {
+    filePath += "fire.png";
+    _rect = sf::IntRect({0, 0}, {16, 32});
+    }
+    _setTexture(filePath);
+} // En théorie ça devrait marcher mais du coup faut setup les intRect aussi je crois ??
+
+void player::_setTexture(std::string filepath)
+{
+    _texture = sf::Texture();
+    _texture.loadFromFile(filepath);
+    _sprite.setTexture(_texture);
+}
+
+void player::_kill()
+{
+    _alive = false;
+    // _playDeathAnimation(); ??
+}
+
+void player::_draw(sf::RenderWindow &window)
+{
+    window.draw(_sprite);
+}
+
+void player::sizeUp()
+{
+    if (_size < 2)
+    _size++;
+    //powerup_animation ?
+    _chooseTexture();
+}
+
+void player::sizeDown()
+{
+    if (_size > 0)
+    _size--;
+    else
+    return _kill();
+    //dmg_animation ?
+    _invincible = true;
+    _chooseTexture();
+} // https://youtube.com/clip/UgkxKB8VUWV1k7F8slDmzFQhL8ufuSxesyAu?si=4pkCQniZGHPhfBQH
+
+void player::giveStar()
+{
+    _invincible = true;
+    _invincibilityDuration = 12.0f; // 400 sec timer = 160 sec et star dure 30 sec timer donc 12 sec
+    _invincibilityClock.restart();
+    //if (!invincibility music) play it
+    //same pour l'anim
+} // https://youtube.com/clip/UgkxPNQt56KE723coeqs2zg6gfIu32_fpotV?si=Dz7iicDGEC9WFhqu
+
+// void player::shoot()
+// {
+    //     if (_size == 2 && KeyPressed = "b") { // sfEvent ? 
+    //         lancer une boule de feu  //ça pourrait être sa classe à part en soit, puisque "tout est classe"
+    //     }
+    // }
 
 void player::_checkInvincibility()
 {
@@ -39,7 +110,6 @@ void player::_checkInvincibility()
         }
     }
 }
-
 
 void player::_handleIdleInput()
 {
@@ -104,69 +174,3 @@ void player::actualize(sf::RenderWindow &window)
         player::_sprite.setScale({1, 1});
     player::_draw(window);
 }
-
-void player::_chooseTexture()
-{
-    std::string filePath = "ressources/player/" + _character + "/";
-
-    if (_size == 0)
-        filePath += "small.png";
-    else if (_size == 1)
-        filePath += "big.png";
-    else if (_size == 2)
-        filePath += "fire.png";
-    _setTexture(filePath);
-} // En théorie ça devrait marcher mais du coup faut setup les intRect aussi je crois ??
-
-void player::_setTexture(std::string filepath)
-{
-    _texture = sf::Texture();
-    _texture.loadFromFile(filepath);
-    _sprite.setTexture(_texture);
-}
-
-void player::_kill()
-{
-    _alive = false;
-    // _playDeathAnimation(); ??
-}
-
-void player::_draw(sf::RenderWindow &window)
-{
-    window.draw(_sprite);
-}
-
-void player::sizeUp()
-{
-    if (_size < 2)
-        _size++;
-    //powerup_animation ?
-    _chooseTexture();
-}
-
-void player::sizeDown()
-{
-    if (_size > 0)
-        _size--;
-    else
-        return _kill();
-    //dmg_animation ?
-    _invincible = true;
-    _chooseTexture();
-} // https://youtube.com/clip/UgkxKB8VUWV1k7F8slDmzFQhL8ufuSxesyAu?si=4pkCQniZGHPhfBQH
-
-void player::giveStar()
-{
-    _invincible = true;
-    _invincibilityDuration = 12.0f; // 400 sec timer = 160 sec et star dure 30 sec timer donc 12 sec
-    _invincibilityClock.restart();
-    //if (!invincibility music) play it
-    //same pour l'anim
-} // https://youtube.com/clip/UgkxPNQt56KE723coeqs2zg6gfIu32_fpotV?si=Dz7iicDGEC9WFhqu
-
-// void player::shoot()
-// {
-//     if (_size == 2 && KeyPressed = "b") { // sfEvent ? 
-//         lancer une boule de feu  //ça pourrait être sa classe à part en soit, puisque "tout est classe"
-//     }
-// }
