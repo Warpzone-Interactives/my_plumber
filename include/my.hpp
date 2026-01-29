6c163d921a99d720fa56a3aa6cff43916eea13d3
Sweton Herbin <sweton.herbin@epitech.eu>
2026-01-29 11:39:36 +0100

    [-] rm old struct for g_stat

--- include/my.hpp
+++ include/my.hpp
@@ -15,18 +15,17 @@
 #include <fstream>
 #include <cstdio>
 #include <iostream>
-#include "struct.hpp"
+#include "block.hpp"
 #include "player.hpp"
+#include "gstat.hpp"
 
-void handle_window(sf::RenderWindow *window, std::vector<sf::Sprite> &sprites, player *player);
+void handle_window(sf::RenderWindow *window, general_stat g_stat, player *player);
 std::vector<std::vector<sf::Vector2f>> create_grid(int x_size);
 void create_level(general_stat *g_stat);
 
 //------------------------------------ init game ------------------------------------
 std::vector<std::string> load_map(std::string filepath);
 int init_error(general_stat g_stat);
-general_stat init_general(char **av);
 std::string get_where(std::string filepath);
-void init_texture(general_stat *g_stat);
 
 #endif
